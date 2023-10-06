4f0efd51f12d6c259c686418607cbdff1f687bf6
TheSkyScout <antonio.wilczynski2006@gmail.com>
2023-10-07 00:01:01 +0200

    swaped to build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,109 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+val githubRepo = "wolkenlosmc/WolkenlosApi"
+
+group = "eu.wolkenlosmc"
+version = "1.0"
+description = "A API for Wolkenlos plugins"
+
+plugins {
+    kotlin("jvm") version "1.8.22"
+    kotlin("plugin.serialization") version "1.8.22"
+
+    `maven-publish`
+    `java-library`
+
+    id("org.jetbrains.dokka") version "1.8.20"
+    id("io.papermc.paperweight.userdev") version "1.5.5"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    paperweight.paperDevBundle("1.19.4-R0.1-SNAPSHOT")
+
+    api("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.7.1")
+}
+
+tasks {
+    assemble {
+        dependsOn(reobfJar)
+    }
+
+    withType<JavaCompile> {
+        options.encoding = "UTF-8"
+        options.release.set(17)
+    }
+
+    withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "17"
+    }
+
+    dokkaHtml.configure {
+        outputDirectory.set(projectDir.resolve("docs"))
+    }
+}
+
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
+publishing {
+    repositories {
+        maven("https://oss.sonatype.org/service/local/staging/deploy/maven2") {
+            name = "ossrh"
+            credentials {
+                username = System.getenv("OSSRH_USERNAME")
+                password = System.getenv("OSSRH_PASSWORD")
+            }
+        }
+        maven(url = "https://maven.pkg.github.com/wolkenlosmc/wolkenlosapi") {
+            name = "GitHubPackages"
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>(project.name) {
+            from(components["java"])
+            artifact(tasks.jar.get().outputs.files.single())
+
+            this.groupId = project.group.toString()
+            this.artifactId = project.name.toLowerCase()
+            this.version = project.version.toString()
+
+            pom {
+                name.set(project.name)
+                description.set(project.description)
+
+                developers {
+                    developer {
+                        name.set("jakobkmar")
+                    }
+                }
+
+                licenses {
+                    license {
+                        name.set("GNU General Public License, Version 3")
+                        url.set("https://www.gnu.org/licenses/gpl-3.0.en.html")
+                    }
+                }
+
+                url.set("https://github.com/${githubRepo}")
+
+                scm {
+                    connection.set("scm:git:git://github.com/${githubRepo}.git")
+                    url.set("https://github.com/${githubRepo}/tree/main")
+                }
+            }
+        }
+    }
+}
