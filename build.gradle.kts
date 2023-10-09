6e0d5917eca8ab70faaf04f7628435965861e2d2
TheSkyScout <antonio.wilczynski2006@gmail.com>
2023-10-09 14:15:18 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 val githubRepo = "wolkenlosmc/WolkenlosApi"
 
 group = "eu.wolkenlosmc"
-version = "1.1"
+version = "1.1.1"
 description = "This is an API for developing plugins for the WolkenlosMC network."
 
 plugins {
